d4acf3f973a893f3762df1b93108c581015c086a
Adodo Daniel <ithinkiminlove99@gmail.com>
2025-07-28 20:08:49 +0100

    fix: typo and error handling

--- app.js
+++ app.js
@@ -1,64 +1,70 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const axion = require("axios");
 const https = require("https");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", function(req,res){
-  res.sendFile(__dirname+"/signup.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function(req,res){
+app.post("/", function (req, res) {
   const fName = req.body.firstName;
   const lName = req.body.lastName;
   const email = req.body.mail;
-  
-  var data = {
+
+  const data = {
     members: [
-       {
+      {
         email_address: email,
         status: "subscribed",
         merge_fields: {
-          FNAME:fName,
-          LNAME:lName
-        }
-       }
-      ]
+          FNAME: fName,
+          LNAME: lName,
+        },
+      },
+    ],
   };
+
   const jsonData = JSON.stringify(data);
-  const url = "https://us3.api.mailchimp.com/3.0/lists/474f13493d"
+
+  const url = "https://us3.api.mailchimp.com/3.0/lists/474f13493d";
+
   const options = {
-    method:"POST",
-    auth: "safe01:" + process.env.MAILCHIMP_API_KEY
+    method: "POST",
+    auth: "safe01:" + process.env.MAILCHIMP_API_KEY,
   };
-  const request = https.request(url,options,function (response){
-    if (response.statusCode === 200){
-    res.sendFile(__dirname+"/success.html");
-  }else{
-    res.sendFile(__dirname+"/failure.html");
-  }
-  
-    response.on("data",function(data){
+
+  const request = https.request(url, options, function (response) {
+    console.log("Status code from Mailchimp:", response.statusCode);
+
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function (data) {
       console.log(JSON.parse(data));
-    })
-  })
+    });
+  });
+
+  request.on("error", function (e) {
+    console.error("Request error:", e);
+    res.sendFile(__dirname + "/failure.html");
+  });
+
   request.write(jsonData);
   request.end();
 });
 
-app.post("/failure", function(req,res){
+app.post("/failure", function (req, res) {
   res.redirect("/");
 });
 
-
-app.listen(process.env.PORT || 3000, function(){
-  console.log("server is currently running on port " + (process.env.PORT || 3000));
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is running on port " + (process.env.PORT || 3000));
 });
-
-//api key: ed2cfbf344707b92c825e3d00ad0ba87-us3
-//list id  474f13493d
-//new api key: ea105b436fd939775e77a0ce557d25d8-us3
